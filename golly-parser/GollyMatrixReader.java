c11ad92e8ba59ec859d731e4207fbb1259830a5c
segfault <sf@sharped.net>
2014-02-27 23:57:47 +0100

    Removed try catch - ex has to the caught by GollyRleReader's compareMatrices() method

--- golly-parser/GollyMatrixReader.java
+++ golly-parser/GollyMatrixReader.java
@@ -6,7 +6,7 @@ public class GollyMatrixReader
     private ArrayList<ArrayList<Integer>> manualMatrix;
     private String manualFile;
 
-    GollyMatrixReader(String manualFile) throws IOException
+    GollyMatrixReader(String manualFile)
     {
 	this.manualMatrix = new ArrayList<ArrayList<Integer>>();
 	this.manualFile = manualFile;
@@ -15,24 +15,16 @@ public class GollyMatrixReader
     /*** Utilities ***/
     public ArrayList<ArrayList<Integer>> parseMatrixFile() throws IOException
     {
-	// read in the data
-	try
+	Scanner inputMatrix = new Scanner(new File(manualFile));
+	while(inputMatrix.hasNextLine())
 	    {
-		Scanner inputMatrix = new Scanner(new File(manualFile));
-		while(inputMatrix.hasNextLine())
+		Scanner colReader = new Scanner(inputMatrix.nextLine());
+		ArrayList<Integer> colMatrix = new ArrayList<Integer>();
+		while(colReader.hasNextInt())
 		    {
-			Scanner colReader = new Scanner(inputMatrix.nextLine());
-			ArrayList<Integer> colMatrix = new ArrayList<Integer>();
-			while(colReader.hasNextInt())
-			    {
-				colMatrix.add(colReader.nextInt());
-			    }
-			manualMatrix.add(colMatrix);
+			colMatrix.add(colReader.nextInt());
 		    }
-	    }
-	catch (IOException e)
-	    {
-		System.out.println("Exception caught: " + e.getMessage());
+		manualMatrix.add(colMatrix);
 	    }
 	return manualMatrix;
     }
