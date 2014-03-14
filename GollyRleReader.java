aa7c10d2397cf5b123e6b5b34c4f139023d762a4
segfault <sf@sharped.net>
2014-03-14 22:42:40 +0100

    parseFile() made throwable, some code cleanup

--- GollyRleReader.java
+++ GollyRleReader.java
@@ -20,70 +20,33 @@ public class GollyRleReader
     }
   }
 
-  public GollyRleConfiguration parseFile(String file)
+  /** Parse a given file and return its configuration */
+  public GollyRleConfiguration parseFile(String file) throws RuntimeException, IOException
   {
-    GollyRleConfiguration config = new GollyRleConfiguration();
-    try
-    {
-      FileInputStream fis = new FileInputStream(file);
-      ANTLRInputStream input = new ANTLRInputStream(fis);
-      BailGollyRleLexer lexer = new BailGollyRleLexer(input);
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      GollyRleParser parser = new GollyRleParser(tokens);
-      parser.getInterpreter().setPredictionMode(PredictionMode.SLL); // faster
-      parser.removeErrorListeners();
-      parser.setErrorHandler(new BailErrorStrategy());
-
-      try
-      {
-        ParseTreeWalker walker = new ParseTreeWalker();
-        GollyRleFileLoader loader = new GollyRleFileLoader();
-        ParseTree tree = parser.rle();
-        walker.walk(loader, tree);
-        config = loader.getConfiguration();
-        System.out.println("Golly RLE format validation: PASSED");
-      }
-      catch (RuntimeException e)
-      {
-        // e.printStackTrace();
-        System.err.println("Exception caught: " + e.getCause());
-	System.out.println("Golly RLE format validation: FAILED");
-      }
-/*    catch (RuntimeException ex)
-      {
-        // try to recover from a RecognEx
-        System.err.println("Exception caught: " + ex.getCause());
-        if (ex.getClass() == RuntimeException.class &&
-            ex.getCause() instanceof RecognitionException)
-        {
-          // The BailErrorStrategy wraps the RecognitionExceptions in
-          // RuntimeExceptions so we have to make sure we're detecting
-          // a true RecognitionException not some other kind
-          tokens.reset(); // rewind input stream
-          // back to standard listeners/handlers
-          parser.addErrorListener(ConsoleErrorListener.INSTANCE);
-          parser.setErrorHandler(new DefaultErrorStrategy());
-          parser.getInterpreter().setPredictionMode(PredictionMode.LL); // try full LL(*)
-          parser.rle();
-        }
-        System.out.println("Golly RLE format validation: FAILED");
-      }
-*/
-    }
-    catch (IOException e)
-    {
-      System.err.println("ERROR: " + e.getMessage());
-    }
-
+    GollyRleConfiguration config = null;
+    FileInputStream fis = new FileInputStream(file);
+    
+    /** Setting up ANTLR */
+    ANTLRInputStream input = new ANTLRInputStream(fis);
+    BailGollyRleLexer lexer = new BailGollyRleLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    GollyRleParser parser = new GollyRleParser(tokens);
+    parser.getInterpreter().setPredictionMode(PredictionMode.SLL); // faster mode
+    parser.removeErrorListeners();
+    parser.setErrorHandler(new BailErrorStrategy());
+    
+    /** Parsing tree */
+    ParseTreeWalker walker = new ParseTreeWalker();
+    GollyRleFileLoader loader = new GollyRleFileLoader();
+    ParseTree tree = parser.rle();
+    walker.walk(loader, tree);
+    
+    /** Getting config */
+    config = loader.getConfiguration();
     return config;
   }
 
-  public boolean parseString(String text)
-  {
-    boolean validString = false;
-    return validString;
-  }
-
+  // TODO: move this method to batcher
   // public static void compareMatrices(GollyRleConfiguration config, String manualMatrixFile) throws IOException
   // {
   //   manualMatrixFile += ".txt"; // rude temporary choice
@@ -118,64 +81,17 @@ public class GollyRleReader
 
   public static void main(String[] args) throws Exception
   {
-
     if(args.length == 0)
     {
-      System.err.println("For a list of the available commands type: GollyRleReader -h");
+      System.err.println("Syntax: GollyRleReader <gollyca.rle>");
       System.exit(1);
     }
     else
     {
       String actualFile = args[0];
-      if (actualFile.equals("-h"))
-      {
-        System.out.println("Syntax\t: GollyRleReader <FILE> [OPTIONS]\n\n"
-			   + " <FILE> is a mandatory field;\n"
-			   + " [OPTIONS] are optional fields:\n"
-			   + "    -d   : draw the matrix\n");
-			   //+ "    -c   : parse FILE(.RLE) and validate the equivalent handwritted FILE(.RLE.TXT) file in the same dir\n");
-      }
-      else
-      {
-        GollyRleReader reader = new GollyRleReader();
-        GollyRleConfiguration config = reader.parseFile(actualFile);
-
-        // Perform other actions if requested
-        if (args.length > 1)
-        {
-          boolean alreadyDrown = false;
-          boolean alreadyCompared = false;
-          for (int i = 1; i < args.length; i++)
-          {
-            //switch(args[i])
-	    switch(1)
-            {
-              //case "-d":
-	      //fking processing
-	    case 1:
-              {
-                if (!alreadyDrown)
-                {
-                  config.drawMatrix();
-                  alreadyDrown = true;
-                }
-                break;
-              }
-              // case "-c":
-              // {
-              //   if (!alreadyCompared)
-              //   {
-	      // 	  compareMatrices(config, actualFile);
-              //     alreadyCompared = true;
-              //   }
-              //   break;
-              // }
-            }
-          }
-        }
-        //System.out.println("FILE is " + (validFile? "VALID": "NOT VALID"));
-      }
+      GollyRleReader reader = new GollyRleReader();
+      GollyRleConfiguration config = reader.parseFile(actualFile);
     }
   }
-
+  
 }
